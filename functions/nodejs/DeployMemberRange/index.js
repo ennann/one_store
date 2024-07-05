1ed622cd8efeff3c59717caa23c1471a531c35c5
Ethan Zhao <hi@yizhe.me>
2024-07-05 14:05:09 +0800

    修复人员查找函数问题

--- functions/nodejs/DeployMemberRange/index.js
+++ functions/nodejs/DeployMemberRange/index.js
@@ -10,15 +10,15 @@
  */
 module.exports = async function (params, context, logger) {
     logger.info(`查找人员范围函数开始执行`, params);
-
+  
     const { user_rule, work_team, user_department, job_position, publisher } = params;
     let userList = [];
-
+  
     if (!user_rule && !work_team && !user_department && !job_position) {
         logger.error('传入参数错误：筛选人员时缺少人员筛选规则');
         return userList;
     }
-
+  
     // 补全人员规则字段的数据
     let ruleRecord = {};
     if (user_rule) {
@@ -39,7 +39,7 @@ module.exports = async function (params, context, logger) {
         }
     }
     logger.info('筛选人员规则：', ruleRecord);
-
+  
     // 原子函数：获取用户记录数据
     const getUserRecord = async (query, description) => {
         try {
@@ -57,7 +57,7 @@ module.exports = async function (params, context, logger) {
             return userList;
         }
     };
-
+  
     // 原子函数：获取店长人员
     const getStoreMangers = async query => {
         try {
@@ -73,7 +73,7 @@ module.exports = async function (params, context, logger) {
             return [];
         }
     };
-
+  
     // 原子函数：获取店员人员
     const getStoreClerks = async query => {
         try {
@@ -89,7 +89,7 @@ module.exports = async function (params, context, logger) {
             return [];
         }
     };
-
+  
     // 原子函数：获取其他职务人员
     const getOtherJobPosition = async query => {
         try {
@@ -105,9 +105,9 @@ module.exports = async function (params, context, logger) {
             return [];
         }
     };
-
+  
     // 获取特定岗位下的所有人员
-    const getMembers = async (roleRecord, job_position) => {
+    const getJobPositionMembers = async (roleRecord, job_position) => {
         let department; // 发布人的部门
         if (roleRecord.role !== 'option_admin') {
             // 不为管理员
@@ -121,40 +121,52 @@ module.exports = async function (params, context, logger) {
             }
             department = userRecord._department;
         }
-    
+  
         // 原子函数：根据岗位记录ID查找岗位数据
         const jobPositionRecords = await application.data
             .object('object_job_position')
-            .select('job_code', '_id', 'source')
+            .select('job_code', '_id', 'source', 'job_name')
             .where({
                 _id: application.operator.hasAnyOf(job_position.map(item => item.id || item._id)),
             })
             .find();
-        logger.info('【日志检查】获取到的岗位记录：', JSON.parse(jobPositionRecords));
-        
-        const funList = jobPositionRecords.map(i => {
+        logger.info('【日志检查】获取到的岗位记录：', jobPositionRecords);
+  
+        const result = [];
+  
+        for (const position of jobPositionRecords) {
+            let res;
+            // logger.info('for循环内的当前岗位信息：', position);
             // 1. 如果岗位信息是手动维护的，就根据店长或者店员找到对应的人员
-            if (i.source === 'option_manual') {
-                if (i.job_code === 'store_clerk') {
-                    return getStoreClerks(department ? { store_staff_department: { _id: department._id } } : {});
+            if (position.source === 'option_manual') {
+                // logger.info('该岗位信息是手动维护的', position);
+                if (position.job_code === 'store_clerk') {
+                    res = await getStoreClerks(department ? { store_staff_department: { _id: department._id } } : {});
                 } else {
-                    return getStoreMangers(department ? { store_department: { _id: department._id } } : {});
+                    res = await getStoreMangers(department ? { store_department: { _id: department._id } } : {});
                 }
+            } else if (position.source === 'option_feishu') {
+                // 2. 如果岗位信息是从飞书同步的，就在用户表里找到对应的人员
+                // logger.info('该岗位信息是从飞书自动同步的', position);
+                res = await getOtherJobPosition({ _jobTitle: position.job_name });
+            } else {
+                res = []; // 添加默认返回值，确保返回的是一个数组
             }
-            // 2. 如果岗位信息是从飞书同步的，就在用户表里找到对应的人员
-            if (i.job_code === 'option_feishu') {
-                return getOtherJobPosition({ _jobTitle: i.job_name });
-            }
-            return Promise.resolve([]); // 添加默认返回值，确保返回的是 Promise
-        });
-        
-        const result = await Promise.all(funList);
-        logger.info('【日志检查】获取到的岗位下的人员：', result);
-        const apaasUserRecordIds = result.flat().map(i => i._id || i.id).filter(id => id);
+            logger.info(`【${position.job_name}岗位检查】获取到的岗位下的人员：`, res);
+            result.push(...res);
+        }
+  
+        // 对 result 内元素的 _id 进行整理，放在 apaasUserRecordIds 内
+        const apaasUserRecordIds = result.map(item => item._id);
+        logger.info('【日志检查】获取到的岗位下的人员ID：', apaasUserRecordIds);
+        if (apaasUserRecordIds.length === 0) {
+            logger.warn('获取到的岗位下的人员为空');
+            return [];
+        }
         const users = await getUserRecord({ _id: application.operator.hasAnyOf(apaasUserRecordIds) }, '所属岗位');
         return users;
     };
-
+  
     // 获取部门多层级下的人员
     const getDepartmentUser = async ids => {
         const list = [];
@@ -172,14 +184,14 @@ module.exports = async function (params, context, logger) {
         }
         return list;
     };
-
+  
     // 获取所属部门下的人员
     if (ruleRecord.department && ruleRecord.department.length > 0) {
         const departmentIds = ruleRecord.department.map(item => item._id || item.id);
         const users = await getDepartmentUser(departmentIds);
         userList.push(...users);
     }
-
+  
     // 获取所属用户组下的人员
     if (ruleRecord.work_team && ruleRecord.work_team.length > 0) {
         const teamIds = ruleRecord.work_team.map(item => item._id || item.id);
@@ -192,10 +204,15 @@ module.exports = async function (params, context, logger) {
                 },
             })
             .find();
-        const users = await getUserRecord({ _id: application.operator.hasAnyOf(teamUserList.map(item => item.user._id)) }, '所属用户组');
-        userList.push(...users);
+        logger.info('获取到的用户组下的人员：', teamUserList);
+        if (teamUserList.length === 0) {
+            logger.warn('获取到的用户组下的人员为空');
+        } else {
+            const users = await getUserRecord({ _id: application.operator.hasAnyOf(teamUserList.map(item => item.user._id)) }, '所属用户组');
+            userList.push(...users);
+        }
     }
-
+  
     // 获取岗位下的人员
     if (ruleRecord.job_position && ruleRecord.job_position.length > 0) {
         let jobUsers = [];
@@ -208,7 +225,7 @@ module.exports = async function (params, context, logger) {
                 .findOne();
             logger.info('【权限检查】发布人权限记录：', roleRecord);
             if (roleRecord) {
-                jobUsers = await getMembers(roleRecord, ruleRecord.job_position);
+                jobUsers = await getJobPositionMembers(roleRecord, ruleRecord.job_position);
             } else {
                 logger.warn('在权限授权明细表没有启用状态的发布人');
             }
@@ -217,17 +234,18 @@ module.exports = async function (params, context, logger) {
         }
         userList.push(...jobUsers);
     }
-
+  
     userList = userList.filter((item, index, self) => self.findIndex(t => item._lark_user_id && t._lark_user_id === item._lark_user_id) === index);
-
+  
     if (userList.length === 0) {
         logger.error('通过人员筛选条件获取人员列表为空');
         return [];
     }
     logger.info('查找人员范围函数执行结束，总共找到人员：', userList.length);
-
+  
     return userList.map(item => ({
         ...item,
         user_id: item._lark_user_id,
     }));
-};
+  };
+  
